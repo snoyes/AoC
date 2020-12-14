b21a4cf91e0ce432f7738ffdd4a6ec8c1b2b91a4
Scott Noyes <snoyes@gmail.com>
2020-12-14 10:03:28 -0600

    Create day14.py

---
+++ 2020/day14.py
@@ -0,0 +1,37 @@
+from aoc import data
+import re
+
+def p(section):
+    mask = section[:36]
+    lines = section.strip().split('\n')[1:]
+    actions = [(int(key),int(val)) for line in lines for key, val in re.findall('(\d+)] = (\d+)', line)]
+    return mask, actions
+
+contents = data(parser=p, delimiter='mask = ')[1:]
+
+def part1(contents):
+    memory = dict()
+    for mask, actions in contents:
+        orMask = int(mask.replace('X', '0'), 2)
+        andMask = int(mask.replace('X', '1'), 2)
+        memory.update( {key: (val | orMask) & andMask for key, val in actions} )
+    return sum(memory.values())
+
+def part2(contents):
+    def swapX(mask, newX):
+        mask = re.split('(X)', mask)
+        mask[1::2] = newX
+        return int(''.join(mask), 2)
+
+    memory = dict()
+    for mask, actions in contents:
+        xCount = mask.count('X')
+        for i in range(2**xCount):
+            newX = bin(i)[2:].zfill(xCount)
+            orMask = swapX(mask, newX)
+            andMask = swapX(mask.replace('0', '1'), newX)
+            memory.update( {(key | orMask) & andMask : val for key, val in actions} )
+    return sum(memory.values())
+
+print(part1(contents))
+print(part2(contents))

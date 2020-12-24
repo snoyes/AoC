440668e94f314d5fa228181a115cfc57dda2c98d
Scott Noyes <snoyes@gmail.com>
2020-12-24 14:04:20 -0600

    No need to save output in variables

--- 2020/day24.py
+++ 2020/day24.py
@@ -10,7 +10,7 @@ directions = { "nw": (-1-1j), "ne": (1-1j), "sw": (-1+1j), "se": (+1+1j), "w": (
 findTile = lambda instruction: sum(directions[i] for i in instruction)
 flippedTiles = Counter(map(findTile, instructions))
 blackTiles = [tile for tile, times in filter(lambda x: x[1] % 2, flippedTiles.items())]
-part1 = len(blackTiles)
+print('Part 1:', len(blackTiles))
 
 findNeighbors = lambda tile: {tile + d for d in directions.values()}
 for day in range(1, 101):
@@ -18,7 +18,4 @@ for day in range(1, 101):
     blackTiles = {tile for tile, n in filter(lambda x: x[1] == 1, neighbors.items())}.intersection(blackTiles).union(
                  {tile for tile, n in filter(lambda x: x[1] == 2, neighbors.items())}
                  )
-part2 = len(blackTiles)
-
-print(part1)
-print(part2)
+print('Part 2:', len(blackTiles))

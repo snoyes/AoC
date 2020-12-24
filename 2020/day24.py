9d064578cf020dce72406f56de1c60287ebde63c
Scott Noyes <snoyes@gmail.com>
2020-12-24 09:04:48 -0600

    new file

---
+++ 2020/day24.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from aoc import data
+import re
+
+parser = lambda x: re.findall('ne|nw|se|sw|e|w', x)
+instructions = data(parser=parser)
+
+directions = { "nw": (-1-1j), "ne": (1-1j), "sw": (-1+1j), "se": (+1+1j), "w": (-2+0j), "e": (+2+0j) }
+
+findTile = lambda instruction: sum(directions[i] for i in instruction)
+flippedTiles = Counter(map(findTile, instructions))
+blackTiles = [tile for tile, times in filter(lambda x: x[1] % 2, flippedTiles.items())]
+part1 = len(blackTiles)
+
+findNeighbors = lambda tile: {tile + d for d in directions.values()}
+stayBlack = lambda x: x[1] == 1
+turnBlack = lambda x: x[1] == 2
+for day in range(1, 101):
+    neighbors = Counter(tile for nearby in map(findNeighbors, blackTiles) for tile in nearby)
+    blackTiles = {tile for tile, n in filter(stayBlack, neighbors.items())}.intersection(blackTiles).union(
+                 {tile for tile, n in filter(turnBlack, neighbors.items())}
+                 )
+part2 = len(blackTiles)
+
+print(part1)
+print(part2)

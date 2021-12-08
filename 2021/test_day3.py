0beb5409ab98405e00b0197797e4d372002dad6b
Scott Noyes <snoyes@gmail.com>
2021-12-08 16:05:42 -0600

    actually tests

--- 2021/test_day3.py
+++ 2021/test_day3.py
@@ -2,10 +2,24 @@ from random import *
 from day3 import *
 
 exampleInput = """
-
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
 """.strip()
 
-exampleData = list(map(parser, exampleInput.split("\n")))
+exampleData = exampleInput.split("\n")
 
 def test_part1_example():
-    assert part1(exampleData) == None
+    assert part1(exampleData) == 198
+
+def test_part2_example():
+    assert part2(exampleData) == 230
